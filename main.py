891e01169fc67d34d86ddf5a76b236a964b26b2e
German <glopez@ciccp.es>
2021-02-05 11:14:50 +0100

    20210206

---
+++ main.py
@@ -0,0 +1,7 @@
+# Solucion Numerica de la Ecuacion de la consolidacion
+# para las condiciones de contorno permeable/permeable y
+# permeable/impermeable
+# Metodo de las Diferencias Finitas
+# Metodo explicito
+# con este programa se obtienen tres archivos de salida para su analisis
+# y cuatro capturas de resultados graficos
