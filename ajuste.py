eb416a0ddb7a22067a1ec47d5fd9cc6329cda42b
German Lopez Pineda <me1lopig@uco.es>
2021-02-12 22:34:13 +0100

    20210214

--- ajuste.py
+++ ajuste.py
@@ -1,1 +1,13 @@
 # ajuste a una funcion desde una lista de puntos
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([2.,3.,4.,5.,6.])
+y = np.array([2.,6.,5.,5.,6.])
+pol = np.polyfit(x,y,len(x)-1)  # coeficientes del polinomio
+
+xx = np.linspace(min(x),max(x))
+yy = np.polyval(pol,xx)         # valor del polinomio en los puntos de la matriz xx
+
+plt.plot(xx, yy, '-',x, y, 'ro')
+#plt.axis([min(xx)-d, max(xx)+d, min(yy)-d, max(yy)+d])
