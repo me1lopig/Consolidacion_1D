9bff6e11aa2f569d625736e459a6dfd34029344c
German Lopez Pineda <me1lopig@uco.es>
2021-02-15 07:44:07 +0100

    modificacion en main

--- main.py
+++ main.py
@@ -1,181 +1,232 @@
+# ejemplo de ventanas
 
+from tkinter import *
 
-import tkinter as tk
-import tkinter.font as tkFont
-
-
-def imprimir (valor):
-    print('El valor es ',valor)
-
-
-print('hola mundo')
-
-
-#declaracion de la ventana
-window = tk.Tk()
-window.title("Introducción de los datos")
-window.geometry('720x600')
-window.state(newstate = "normal")
-window.resizable(False,False) # se evita el modificar las dimensiones de la ventana
+def sumar():
+    # funcion de prueba
+    print('Holi')
 
 
 
+# definición de la ventana
+raiz=Tk()
+raiz.title("Aproximación de la Ecuación de la Consolidación mediante el método de Diferencias Finitas")
+raiz.geometry('720x550') # dimensiones de la ventana
+raiz.state(newstate = "normal")
+raiz.resizable(False,False) # se evita el modificar las dimensiones de la ventana (ancho, alto)
 
 
 #colocacion de las etiqueta de entrada de datos de espesor del estrato
 # encabezadp
-lbl_geometria=tk.Label(window, text="Aproximación de la Ecuación de la Consolidación mediante el método de Diferencias Finitas")
+lbl_geometria=Label(raiz, text="Aproximación de la Ecuación de la Consolidación mediante el método de Diferencias Finitas")
 lbl_geometria.place(x=20,y=10)
 lbl_geometria.pack()
 lbl_geometria.config(font=('Arial', 17)) #Cambiar tipo y tamaño de fuente
 lbl_geometria.config(fg="black") #Cambiar color del texto
 
 
-
-lbl_cartel=tk.Label(window, text="Entrada de datos, para un suelo homogéneo")
+lbl_cartel=Label(raiz, text="Caso de carga uniformemente distribuda")
 lbl_cartel.place(x=20,y=40)
 lbl_cartel.pack()
 lbl_cartel.config(font=('Arial', 15)) #Cambiar tipo y tamaño de fuente
 lbl_cartel.config(fg="black") #Cambiar color del texto
 
 
-lbl_geometria=tk.Label(window, text="Características de cargas y geometría")
-lbl_geometria.place(x=20,y=75)
+
+# entrada de los datos de las condiciones de los parametros del terreno
+cargasGeometria=Frame() # declaramos un frame como contenedor
+cargasGeometria.pack() # empaquetado del frame 
+cargasGeometria.place(x=20,y=75) # posicionamiento del frame
+cargasGeometria.config(width='550',height='90') # dimensiones del frame
+cargasGeometria.config(bd='2.5') # dimensiones del borde
+cargasGeometria.config(cursor='hand2')
+cargasGeometria.config(relief='groove')
 
 
-lbl_espesor=tk.Label(window, text="Espesor del estrato")
-lbl_espesor.place(x=20,y=100)
+
+lbl_geometria=Label(cargasGeometria, text="Características de cargas y geometría")
+lbl_geometria.place(x=20,y=0)
+
+
+lbl_espesor=Label(cargasGeometria, text="Espesor del estrato")
+lbl_espesor.place(x=20,y=25)
 
 #caja de texto de espesor del estrato
 
-T_espesor=tk.StringVar()
-txt_espesor = tk.Entry(window, width=5,textvariable=T_espesor)
-txt_espesor.place(x=350, y=100)
+T_espesor=StringVar()
+txt_espesor = Entry(cargasGeometria, width=5,textvariable=T_espesor)
+txt_espesor.place(x=350, y=25)
 espesor = T_espesor.get()
 
 
-
 #unidades de entrada
-lbl_Uespesor=tk.Label(window, text=" [m]")
-lbl_Uespesor.place(x=400,y=100)
+lbl_Uespesor=Label(cargasGeometria, text=" [m]")
+lbl_Uespesor.place(x=400,y=25)
 
 #colocacion de las etiqueta de entrada de datos de espesor de carga exterior
-lbl_carga=tk.Label(window, text="Carga exterior")
-lbl_carga.place(x=20,y=125)
+lbl_carga=Label(cargasGeometria, text="Carga exterior")
+lbl_carga.place(x=20,y=50)
 
 #caja de texto de la carga exterior
-T_carga=tk.StringVar()
-txt_carga = tk.Entry(window, width=5,textvariable=T_carga)
-txt_carga.place(x=350, y=125)
+T_carga=StringVar()
+txt_carga = Entry(cargasGeometria, width=5,textvariable=T_carga)
+txt_carga.place(x=350, y=50)
 carga = T_carga.get()
 
 #unidades de entrada
-lbl_Ucarga=tk.Label(window, text=" [kPa]")
-lbl_Ucarga.place(x=400,y=125)
+lbl_Ucarga=Label(cargasGeometria, text=" [kPa]")
+lbl_Ucarga.place(x=400,y=50)
 
 
-lbl_terreno=tk.Label(window, text="Parámetros del terreno")
-lbl_terreno.place(x=20,y=175)
+
+
+
+
+# entrada de los datos de las condiciones de los parametros del terreno
+parametrosTerreno=Frame() # declaramos un frame como contenedor
+parametrosTerreno.pack() # empaquetado del frame 
+parametrosTerreno.place(x=20,y=175) # posicionamiento del frame
+parametrosTerreno.config(width='550',height='95') # dimensiones del frame
+parametrosTerreno.config(bd='2.5') # dimensiones del borde
+parametrosTerreno.config(cursor='hand2')
+parametrosTerreno.config(relief='groove')
+
+
+lbl_terreno=Label(parametrosTerreno, text="Parámetros del terreno")
+lbl_terreno.place(x=20,y=0)
 #colocacion de las etiqueta de entrada de datos del terreno,coeficiente de consolidación
 
-lbl_cv=tk.Label(window, text="Coeficiente de consolidacion ")
-lbl_cv.place(x=20,y=200)
+lbl_cv=Label(parametrosTerreno, text="Coeficiente de consolidacion ")
+lbl_cv.place(x=20,y=25)
 
 #caja de texto de entrada
-T_cv=tk.StringVar()
-txt_cv = tk.Entry(window, width=5,textvariable=T_cv)
-txt_cv.place(x=350, y=200)
+T_cv=StringVar()
+txt_cv = Entry(parametrosTerreno, width=5,textvariable=T_cv)
+txt_cv.place(x=350, y=25)
 cv = T_cv.get()
 
 #unidades de entrada
-lbl_Ucv=tk.Label(window, text=" [m2/día]")
-lbl_Ucv.place(x=400,y=200)
+lbl_Ucv=Label(parametrosTerreno, text=" [m2/día]")
+lbl_Ucv.place(x=400,y=25)
 
 
 
-#colocacion de las etiqueta de entrada valor del coeiciente de comprsibilidad volumetrivo
+#colocacion de las etiqueta de entrada valor del coeficiente de comprsibilidad volumetrivo
 
-lbl_mv=tk.Label(window, text="Coeficiente de compresibilidad volumétrica")
-lbl_mv.place(x=20,y=225)
+lbl_mv=Label(parametrosTerreno, text="Coeficiente de compresibilidad volumétrica")
+lbl_mv.place(x=20,y=50)
 
 #caja de texto de entrada
-T_mv=tk.StringVar()
-txt_mv = tk.Entry(window, width=5,textvariable=T_mv)
-txt_mv.place(x=350, y=225)
+T_mv=StringVar()
+txt_mv = Entry(parametrosTerreno, width=5,textvariable=T_mv)
+txt_mv.place(x=350, y=50)
 mv = T_mv.get()
 
 #unidades de entrada
-lbl_Umv=tk.Label(window, text=" [m2/kN]")
-lbl_Umv.place(x=400,y=225)
+lbl_Umv=Label(parametrosTerreno, text=" [m2/kN]")
+lbl_Umv.place(x=400,y=50)
+
+
+
+
+
+
+# entrada de los datos de entrada de las condiciones de contorno
+datosMallado=Frame() # declaramos un frame
+datosMallado.pack() # empaquetado del frame 
+datosMallado.place(x=20,y=275) # posicionamiento del frame
+datosMallado.config(width='550',height='120') # dimensiones del frame
+datosMallado.config(bd='2.5') # dimensiones del borde
+datosMallado.config(cursor='hand2')
+datosMallado.config(relief='groove')
 
 
 #caracterización de la malla de cálculo
-lbl_geometria_malla=tk.Label(window, text="Datos del mallado del modelo")
-lbl_geometria_malla.place(x=20,y=275)
+lbl_geometria_malla=Label(datosMallado, text="Datos del mallado del modelo")
+lbl_geometria_malla.place(x=20,y=0)
 
 # Datos de la malla en x
 
-lbl_x=tk.Label(window, text="Incremento de la malla en x")
-lbl_x.place(x=20,y=300)
+lbl_x=Label(datosMallado, text="Incremento de la malla en x")
+lbl_x.place(x=20,y=25)
 
 #caja de texto de entrada de los valores del espaciado en x
-T_x=tk.StringVar()
-txt_x = tk.Entry(window, width=5,textvariable=T_x)
-txt_x.place(x=350, y=300)
+T_x=StringVar()
+txt_x = Entry(datosMallado, width=5,textvariable=T_x)
+txt_x.place(x=350, y=25)
 x = T_x.get()
 
 #unidades de entrada
-lbl_Ux=tk.Label(window, text=" [m]")
-lbl_Ux.place(x=400,y=300)
+lbl_Ux=Label(datosMallado, text=" [m]")
+lbl_Ux.place(x=400,y=25)
 
 
 
 # Datos de la malla en t
 
-lbl_t=tk.Label(window, text="Incremento de la malla en t")
-lbl_t.place(x=20,y=325)
+lbl_t=Label(datosMallado, text="Incremento de la malla en t")
+lbl_t.place(x=20,y=50)
 
 #caja de texto de entrada de los valores del espaciado en t
-T_t=tk.StringVar()
-txt_t = tk.Entry(window, width=5,textvariable=T_t)
-txt_t.place(x=350, y=325)
+T_t=StringVar()
+txt_t = Entry(datosMallado, width=5,textvariable=T_t)
+txt_t.place(x=350, y=50)
 x = T_t.get()
 
 #unidades de entrada
-lbl_Ut=tk.Label(window, text=" [días]")
-lbl_Ut.place(x=400,y=325)
-
-
-# comprobación de la convergencia del modelo
-lbl_convergencia=tk.Label(window, text="Convergencia del modelo")
-lbl_convergencia.place(x=20,y=350)
-
-lbl_Cconvergencia=tk.Label(window, text='El valor del coeficiente es ')
-lbl_Cconvergencia.place(x=350,y=350)
-
-lbl_Cconvergencia=tk.Label(window, text="El modelo es convergente")
-lbl_Cconvergencia.place(x=475,y=350)
+lbl_Ut=Label(datosMallado, text=" [días]")
+lbl_Ut.place(x=400,y=50)
 
 
 
+# entrada de los datos de entrada de las condiciones de contorno
+condicionesContorno=Frame() # declaramos un frame
+condicionesContorno.pack() # empaquetado del frame 
+condicionesContorno.place(x=20,y=400) # posicionamiento del frame
+condicionesContorno.config(width='350',height='120') # dimensiones del frame
+condicionesContorno.config(bd='2.5') # dimensiones del borde
+condicionesContorno.config(cursor='hand2')
+condicionesContorno.config(relief='groove')
 
 
-# Selección de las condiciones de contorno mediante un radiobutton
-lbl_tipocontorno=tk.Label(window, text="Selección de las condiciones de contorno")
-lbl_tipocontorno.place(x=20,y=400)
 
-radioValue = tk.IntVar() 
+# Selección de las condiciones de contorno mediante un radiobutton dentro del Frame
+lbl_tipocontorno=Label(condicionesContorno, text="Selección de las condiciones de contorno")
+lbl_tipocontorno.place(x=0,y=0)
 
-rdioOne = tk.Radiobutton(window, text='Permeable-Permeable',variable=radioValue, value=1) 
-rdioTwo = tk.Radiobutton(window, text='Permeable-Impermeable',variable=radioValue, value=2) 
-rdioThree = tk.Radiobutton(window, text='Impermeable-Permeable',variable=radioValue, value=3)
+radioValue = IntVar() 
 
+rdioOne = Radiobutton(condicionesContorno, text='Permeable-Permeable',variable=radioValue, value=1) 
+rdioTwo = Radiobutton(condicionesContorno, text='Permeable-Impermeable',variable=radioValue, value=2) 
+rdioThree = Radiobutton(condicionesContorno, text='Impermeable-Permeable',variable=radioValue, value=3)
+rdioOne.select
 # colocacion de los radiobutton
-rdioOne.place(x=20, y=425)
-rdioTwo.place(x=20, y=450)
-rdioThree.place(x=20, y=475)
+rdioOne.place(x=20, y=25)
+rdioTwo.place(x=20, y=50)
+rdioThree.place(x=20, y=75)
 
 
+# Colocación del Frame para colocar los botones de ejecucion
+
+# Frame de la entrada de los botones
+botones=Frame() # declaramos un frame
+botones.pack() # empaquetado del frame 
+botones.place(x=400,y=400) # posicionamiento del frame
+botones.config(width='170',height='120') # dimensiones del frame
+botones.config(bd='2.5') # dimensiones del borde
+botones.config(cursor='hand2')
+botones.config(relief='groove')
+
+#definición de los botones
+
+boton_ejecuta=Button(botones, text="Calcular", command=sumar)
+boton_ejecuta.place(x=40,y=10)
+boton_ejecuta.config(width='8',height='2') # dimensiones del boton
+
+#cerramos la aplicación
+boton_salir=Button(botones, text="Salir", command=raiz.destroy)
+boton_salir.place(x=40,y=55)
+boton_salir.config(width='8',height='2') # dimensiones del boton
 
 
-window.mainloop()
+raiz.mainloop() # mantenemos la ventana abierta en un bucle 
